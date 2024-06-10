5fcad25987cb4826d914d40749addeb60f061b45
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 17:14:08 -0300

    chore: Update go.mod and go.sum with new dependencies

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module mBlockConverter
 go 1.22.3
 
 require (
+	github.com/micmonay/keybd_event v1.1.2
+	go.bug.st/serial v1.6.2
+)
+
+require (
 	github.com/creack/goselect v0.1.2 // indirect
-	github.com/micmonay/keybd_event v1.1.2 // indirect
-	go.bug.st/serial v1.6.2 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 )
--- main.go
+++ main.go
@@ -7,35 +7,57 @@ import (
 	"os"
 )
 
+// Função principal
 func main() {
+	// Definindo cores ANSI
+	reset := "\033[0m"
+	blue := "\033[34m"
+	cyan := "\033[36m"
+	yellow := "\033[33m"
+	green := "\033[32m"
+	red := "\033[31m"
 
+	// Cabeçalho
+	fmt.Println(yellow + "-------------------------------" + reset)
+	fmt.Println(blue + "Linkedin: " + cyan + "https://www.linkedin.com/in/emesdev/" + reset)
+	fmt.Println(blue + "Software feito por: " + cyan + "https://github.com/EmesDev" + reset)
+	fmt.Println(blue + "Licença MIT" + reset)
+	fmt.Println(yellow + "-------------------------------" + reset)
+
+	// Obtendo e listando as portas seriais
 	services.GetPorts()
-
+	fmt.Println(green + "Portas seriais disponíveis:" + reset)
 	for _, port := range services.SerialPorts {
-		fmt.Println(port)
+		fmt.Println(cyan + port + reset)
 	}
+	fmt.Println(yellow + "-------------------------------" + reset)
 
+	// Solicitando entrada do usuário
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Software feito por:  https://github.com/EmesDev")
-	fmt.Print("Escreva a porta serial em caixa alta (Ex: COM4): ")
+	fmt.Print(yellow + "Escreva a porta serial em caixa alta (Ex: COM4): " + reset)
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		fmt.Println(red+"Erro ao ler a entrada:", err.Error()+reset)
 		return
 	}
 
+	// Removendo espaços e nova linha do input
 	input = input[:len(input)-2]
-	fmt.Println("You entered:", input)
+	fmt.Println(green + "Você selecionou a porta: " + cyan + input + reset)
 
+	// Criando uma nova conexão
 	connection := services.NewConnection(115200, input)
 
+	// Inicializando a conexão
 	err = connection.InitConnection()
 	if err != nil {
-		panic(err)
+		fmt.Println(red+"Erro ao iniciar a conexão:", err.Error()+reset)
+		return
 	}
 
+	// Lendo dados da conexão
 	buff := make([]byte, 5)
-
 	connection.Read(buff)
 
+	fmt.Println(green + "Dados lidos: " + cyan + string(buff) + reset)
 }
